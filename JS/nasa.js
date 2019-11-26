2542fc5c15f50b5c8c45f58a9e32f71f1044aa4f
Floor <floorsegers@audience-advantage.com>
2019-11-26 12:19:47 +0100

    add explanation

--- JS/nasa.js
+++ JS/nasa.js
@@ -1,5 +1,7 @@
 var APIkey = "uWIu8uv5Spy9wyE79E7UI6QzuSi1Seu9sX0ZYwaU";
 var nasaImg = document.getElementById("Planets");
+var explanationNASA = document.getElementById("explanation");
+
 
 
 
@@ -13,6 +15,7 @@ var nasaImg = document.getElementById("Planets");
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=${APIkey}`)
       .then(function (response) {
         nasaImg.src = response.data.hdurl;
+        explanationNASA.innerHTML =response.data.explanation;
 
 
         // handle success
